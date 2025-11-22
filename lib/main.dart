d703db2d1c60327082633a8730037c10ad4a2f3e
Luka Meunier <luka.meunier@univ-rouen.fr>
2025-11-22 14:57:28 +0100

    feat(game): implement a first version of core blackjack gameplay and UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:blackjack/models/card.dart' as playing_card;
+import 'package:blackjack/models/player.dart';
 import 'package:blackjack/models/rank.dart';
 import 'package:flutter/material.dart';
 import 'models/deck.dart';
@@ -16,8 +18,9 @@ class MyApp extends StatelessWidget {
       title: 'Blackjack',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Blackjack Deck'),
+      home: const MyHomePage(title: 'Blackjack'),
     );
   }
 }
@@ -32,12 +35,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Deck _deck = Deck();
+  late Deck _deck;
+  late Player _player;
+  late Player _dealer;
 
-  void _shuffleDeck() {
+  @override
+  void initState() {
+    super.initState();
+    _startGame();
+  }
+
+  void _startGame() {
     setState(() {
       _deck = Deck();
+      _player = Player();
+      _dealer = Player();
+
+      // Deal initial cards
+      _player.addCard(_deck.drawCard());
+      _dealer.addCard(_deck.drawCard());
+      _player.addCard(_deck.drawCard());
+      _dealer.addCard(_deck.drawCard());
     });
+  }
+
+  void _hit() {
+    if (_player.score < 21) {
+      setState(() {
+        _player.addCard(_deck.drawCard());
+      });
+    }
   }
 
   @override
@@ -47,28 +74,79 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-        ),
-        itemCount: _deck.cards.length,
-        itemBuilder: (context, index) {
-          final card = _deck.cards[index];
-          return Card(
-            child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            _buildHand('Dealer', _dealer),
+            const SizedBox(height: 24),
+            _buildHand('Player', _player),
+            const Spacer(),
+            if (_player.score >= 21)
+              Text(
+                _player.score > 21 ? 'Bust!' : 'Blackjack!',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            const SizedBox(height: 24),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(card.rank.displayValue.toString()),
-                Icon(card.suit.icon),
+                ElevatedButton(onPressed: _player.score < 21 ? _hit : null, child: const Text('Hit')),
+                const SizedBox(width: 16),
+                ElevatedButton(onPressed: () {}, child: const Text('Stand')),
               ],
             ),
-          );
-        },
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _shuffleDeck,
-        tooltip: 'Shuffle',
-        child: const Icon(Icons.shuffle),
+        onPressed: _startGame,
+        tooltip: 'New Game',
+        child: const Icon(Icons.refresh),
+      ),
+    );
+  }
+
+  Widget _buildHand(String title, Player player) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '$title (Score: ${player.score})',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        const SizedBox(height: 8),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: player.hand.map((card) => CardView(card: card)).toList(),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CardView extends StatelessWidget {
+  const CardView({super.key, required this.card});
+
+  final playing_card.Card card;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      margin: const EdgeInsets.only(right: 8),
+      child: SizedBox(
+        width: 80,
+        height: 110,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(card.rank.displayValue.toString(), style: Theme.of(context).textTheme.titleLarge),
+            Icon(card.suit.icon, size: 30),
+          ],
+        ),
       ),
     );
   }
