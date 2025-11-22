bf2300d8a80cc1af67ad4932115533ef14bb4440
Luka Meunier <luka.meunier@univ-rouen.fr>
2025-11-22 14:47:48 +0100

    feat(models): add Player class for game logic

---
+++ lib/models/player.dart
@@ -0,0 +1,31 @@
+import 'card.dart';
+import 'rank.dart';
+
+class Player {
+  final List<Card> _hand = [];
+
+  List<Card> get hand => List.unmodifiable(_hand);
+
+  void addCard(Card card) {
+    _hand.add(card);
+  }
+
+  int get score {
+    int total = 0;
+    int aceCount = 0;
+
+    for (final card in _hand) {
+      total += card.rank.value;
+      if (card.rank == Rank.ace) {
+        aceCount++;
+      }
+    }
+
+    while (total <= 11 && aceCount > 0) {
+      total += 10;
+      aceCount--;
+    }
+
+    return total;
+  }
+}
