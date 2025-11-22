5920f075e29d1a12854ca214cac52c1ba6922ee1
Luka Meunier <luka.meunier@univ-rouen.fr>
2025-11-22 22:10:42 +0100

    feat(view): overhaul card design for a professional look

--- lib/views/card_view.dart
+++ lib/views/card_view.dart
@@ -1,4 +1,7 @@
+import 'dart:math' as math;
+
 import 'package:blackjack/models/card.dart' as playing_card;
+import 'package:blackjack/models/suit.dart';
 import 'package:flutter/material.dart';
 
 class HiddenCardView extends StatelessWidget {
@@ -8,12 +11,29 @@ class HiddenCardView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 80,
-      height: 110,
+      height: 120,
       margin: const EdgeInsets.only(right: 8),
       decoration: BoxDecoration(
-        color: Colors.blueGrey,
         borderRadius: BorderRadius.circular(8),
-        border: Border.all(color: Colors.black54),
+        border: Border.all(color: Colors.black87, width: 1.5),
+        gradient: const LinearGradient(
+          colors: [Color(0xFF0D47A1), Color(0xFF1976D2)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        boxShadow: const [
+          BoxShadow(blurRadius: 4, offset: Offset(1, 2), color: Colors.black26),
+        ],
+      ),
+      child: Center(
+        child: Container(
+          width: 42,
+          height: 70,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            border: Border.all(color: Colors.white70, width: 1),
+          ),
+        ),
       ),
     );
   }
@@ -26,23 +46,76 @@ class CardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
+    final isRed = card.suit == Suit.hearts || card.suit == Suit.diamonds;
+    final color = isRed ? Colors.red[700]! : Colors.black87;
+
+    return Container(
+      width: 80,
+      height: 120,
       margin: const EdgeInsets.only(right: 8),
-      child: SizedBox(
-        width: 80,
-        height: 110,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              card.rank.shortName,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            Icon(card.suit.icon, size: 30),
-          ],
-        ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: Colors.black87, width: 1.5),
+        boxShadow: const [
+          BoxShadow(blurRadius: 4, offset: Offset(1, 2), color: Colors.black26),
+        ],
       ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 6,
+            left: 6,
+            child: _CornerLabel(
+              rank: card.rank.shortName,
+              icon: card.suit.icon,
+              color: color,
+            ),
+          ),
+          Positioned(
+            bottom: 6,
+            right: 6,
+            child: Transform.rotate(
+              angle: math.pi,
+              child: _CornerLabel(
+                rank: card.rank.shortName,
+                icon: card.suit.icon,
+                color: color,
+              ),
+            ),
+          ),
+          Center(child: Icon(card.suit.icon, size: 36, color: color)),
+        ],
+      ),
+    );
+  }
+}
+
+class _CornerLabel extends StatelessWidget {
+  const _CornerLabel({
+    required this.rank,
+    required this.icon,
+    required this.color,
+  });
+
+  final String rank;
+  final IconData icon;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          rank,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: color,
+          ),
+        ),
+        Icon(icon, size: 16, color: color),
+      ],
     );
   }
 }
